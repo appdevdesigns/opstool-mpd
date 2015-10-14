389bccd8659bd54c690efb3d44e671d6ec8f1d96
ZizZazZuz <josh.s.b97@gmail.com>
2015-10-14 16:31:30 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+opstool-mpd ![Build Status](https://travis-ci.org/appdevdesigns/opstool-mpd.png)
+================
+
+A new plugin.
---
+++ config/routes.js
@@ -0,0 +1,29 @@
+/**
+ * Routes
+ *
+ * Use this file to add any module specific routes to the main Sails
+ * route object.
+ */
+
+
+module.exports = {
+
+
+  /*
+
+  '/': {
+    view: 'user/signup'
+  },
+  '/': 'opstool-mpd/PluginController.inbox',
+  '/': {
+    controller: 'opstool-mpd/PluginController',
+    action: 'inbox'
+  },
+  'post /signup': 'opstool-mpd/PluginController.signup',
+  'get /*(^.*)' : 'opstool-mpd/PluginController.profile'
+
+  */
+
+
+};
+
